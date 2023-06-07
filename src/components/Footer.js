ffb0be059773f53bcb37aea7a11567cbfad2abdf
Sotatek-VinhPham2 <vinh.pham2@sotatek.com>
2023-06-07 10:37:48 +0700

    update

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "../App.css"; 
-
-const Footer = () => {
-  return (
-    <div className="ftr">Tested by <a href='https://vihnpalm.com'>vplm</a></div>
-  )
-}
-export default Footer
